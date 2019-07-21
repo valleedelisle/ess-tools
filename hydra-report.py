90a5940779b1c6fc2854f12612abfa36bb62daca
David Vallee Delisle <dvd@redhat.com>
2019-07-21 17:17:53 -0400

    Generating the report

--- hydra-report.py
+++ hydra-report.py
@@ -7,6 +7,7 @@ Report tool for Red Hat's cases
 
 from __future__ import print_function
 from datetime import datetime, timedelta
+from collections import defaultdict
 import argparse
 import unicodedata  # pylint: disable=unused-import
 import persistent.dict # pylint: disable=unused-import
@@ -48,16 +49,28 @@ def gen_report(args): # pylint: disable=redefined-outer-name
   back_time = datetime.now() - timedelta(hours=args.time)
   CASE_DB = Zoo(CONF.notifierd['database']) # pylint: disable=redefined-outer-name
   LOG.info("{0} cases in memory".format(len(CASE_DB.root["cases"])))
+  event_customer = defaultdict(dict)
   for case in CASE_DB.root['cases']:
     events = CASE_DB.root['cases'][case].events
+    customer = CASE_DB.root['cases'][case].customer
     if events:
       filtered_events = sorted(list(filter(lambda x: x.time >= back_time, events)),
                                key=lambda x: x.time, reverse=True)
       if filtered_events:
-        LOG.info("Case %s" % case)
+        LOG.info("Case %s Customer %s" % (case, customer))
+        if customer not in event_customer:
+          event_customer[customer] = defaultdict(list)
         for event in filtered_events:
-          LOG.info("%s %s" % (event.time, event.text))
+          event_customer[customer][case].append(event)
 
+  for customer in event_customer:
+    customer_conf = getattr(CONF, customer)
+    body = "These were the case events for {0} for the last {1} hours:\n".format(customer_conf['name'], args.time)
+    for case in event_customer[customer]:
+      body += "Case {0} Status: {1}\n".format(case, event_customer[customer][case][0].case.status)
+      for event in event_customer[customer][case]:
+        body += "- {0} {1}\n".format(event.time, event.text)
+    print(body)
   CASE_DB.close()
 
 if __name__ == '__main__':
