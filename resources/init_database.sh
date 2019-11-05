ad0192d997d3c9c61d6f47b5af9f34e71cc8ebff
David Vallee Delisle <dvd@redhat.com>
2019-11-04 23:01:09 -0500

    Adding shebang

--- resources/init_database.sh
+++ resources/init_database.sh
@@ -1,2 +1,3 @@
+#!/bin/bash 
 alembic revision --autogenerate -m "init"                                                                                                                                                                                                   
 alembic upgrade head
