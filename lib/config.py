3d9f8350d5231704a7b762be65c95c62718ddf4c
David Vallee Delisle <dvd@redhat.com>
2019-11-06 15:08:46 -0500

    Moving the environ hack to config lib

--- lib/config.py
+++ lib/config.py
@@ -3,7 +3,8 @@ Returns the configuration object
 """
 
 import configparser
-import os
+from os import environ
+import re
 
 class CaseConfigParser(configparser.SafeConfigParser):# pylint: disable=too-many-ancestors
   """
@@ -18,7 +19,13 @@ class Config(object): # pylint: disable=useless-object-inheritance,too-few-publi
   Returns the configuration object
   """
   def __init__(self, config_file=None):
-    config = CaseConfigParser(os.environ)
+    # Since we use dynamic service names, and configparser doesn't support nested interpolation
+    # we need to rewrite environment to set MARIADB_SERVICE_HOST and MARIADB_SERVICE_PORT
+    for key in environ:
+      match = re.match('.*(MARIADB_SERVICE_(HOST|PORT))', key)
+      if match:
+        environ[match.group(1)] = environ[key]
+    config = CaseConfigParser(environ)
     config.read(config_file)
     self.__dict__.update(config)
     self.configparser = config
