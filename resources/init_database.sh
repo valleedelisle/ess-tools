ffebf96d227817ad6f11044aad8a5721e4d9d809
David Vallee Delisle <dvd@redhat.com>
2020-02-05 08:59:55 -0500

    Checking if maria is up before populating

--- resources/init_database.sh
+++ resources/init_database.sh
@@ -1,3 +1,7 @@
 #!/bin/bash 
+while ! echo > /dev/tcp/${MARIADB_SERVICE_HOST}/3306; do
+  echo "Rundeck still not up"
+  sleep 10
+done
 alembic revision --autogenerate -m "${OPENSHIFT_BUILD_NAME} ${OPENSHIFT_BUILD_REFERENCE} ${OPENSHIFT_BUILD_COMMIT}"
 alembic upgrade head
